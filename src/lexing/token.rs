76970ab919b83c689b81d7053e34cd0b21b6c971
Abhinav Srivastav <abhinavsrivastav25098@gmail.com>
2025-11-14 19:12:13 +0530

    initial commit

---
+++ src/lexing/token.rs
@@ -0,0 +1,78 @@
+#[derive(Debug)]
+pub enum Token {
+    
+    // keywords
+    Print(String),
+    If(String),
+    Else(String),
+    Int(String),
+
+    // literals
+    IntegerLiteral(i32),
+    StringLiteral(String),
+
+    // identifiers
+    Identifier(String),
+
+    // operators 
+    Plus(String),
+    Assign(String),
+
+    // punctuation
+    SemiColon(String),
+    LeftParen(String),
+    RightParen(String),
+    LeftBrace(String),
+    RightBrace(String),
+
+    // logical operators
+    GreaterThan(String),
+    LessThan(String),
+}
+
+impl Token {
+    pub fn get_token(token_type: &str, value: Option<&str>) -> Token {
+        match token_type {
+            "Print" => Token::Print("print".to_string()),
+            "If" => Token::If("if".to_string()),
+            "Else" => Token::Else("else".to_string()),
+            "Int" => Token::Int("int".to_string()),
+            "IntegerLiteral" => Token::IntegerLiteral(value.unwrap().parse::<i32>().unwrap()),
+            "StringLiteral" => Token::StringLiteral(value.unwrap().to_string()),
+            "Identifier" => Token::Identifier(value.unwrap().to_string()),
+            "Plus" => Token::Plus("+".to_string()),
+            "Assign" => Token::Assign("=".to_string()),
+            "SemiColon" => Token::SemiColon(";".to_string()),
+            "LeftParen" => Token::LeftParen("(".to_string()),
+            "RightParen" => Token::RightParen(")".to_string()),
+            "LeftBrace" => Token::LeftBrace("{".to_string()),
+            "RightBrace" => Token::RightBrace("{".to_string()),
+            "GreaterThan" => Token::GreaterThan(">".to_string()),
+            "LessThan" => Token::LessThan("<".to_string()),
+            _ => panic! ("invalid token type {}", token_type),
+
+        }
+    }
+
+    pub fn get_token_regex(token_type: &str) -> String {
+        match token_type {
+            "Print" => r"print",
+            "If" => r"if",
+            "Else" => r"else",
+            "Int" => r"int\s+",
+            "IntegerLiteral" => r"\d+",
+            "StringLiteral" => r#"\".*\""#,
+            "Identifier" => r"[a-zA-Z_][a-zA-Z0-9_]* =",
+            "Plus" => r"\+",
+            "Assign" => r"=",
+            "SemiColon" => r";",
+            "LeftParen" => r"\(",
+            "RightParen" => r"\)",
+            "LeftBrace" => r"\{",
+            "RightBrace" => r"\}",
+            "GreaterThan" => r">",
+            "LessThan" => r"<",
+            _ => panic!("invalid token type: {}", token_type),
+        }.to_string()
+    }
+}
